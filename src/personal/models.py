10cda19853dd239abbfd6b9cbaefd609fa75de2f
Prota <Abdelbadi5@protonmail.com>
2024-10-15 11:42:33 +0200

    Data and models test

--- src/personal/models.py
+++ src/personal/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+PRIORITY = [
+    ("H", "High"),
+    ("M", "Medium"),
+    ("L", "Low"),
+    
+]
+
+class Question(models.Model):
+    title                   = models.CharField(max_length=120)
+    question                = models.TextField(max_length=400)
+    priority                = models.CharField(max_length=1, choices=PRIORITY)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta :
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+        
