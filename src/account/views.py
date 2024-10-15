7642d1795fff2c91c745ba3cbe1b739eb447f30a
Prota <Abdelbadi5@protonmail.com>
2024-10-15 14:43:21 +0200

    Adding Logging options

--- src/account/views.py
+++ src/account/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth import login, authenticate , logout
-
-from account.forms import RegistrationForm
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 # Create your views here.
 
@@ -27,3 +26,27 @@ def registration_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+def login_view(request):
+    context = {}
+    
+    user = request.user
+    if user.is_authenticated:
+        return redirect('home')
+    
+    if request.POST:
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST['email']
+            password = request.POST['password']
+            user = authenticate(email=email, password=password)
+            
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AccountAuthenticationForm()
+        
+    context['login_form'] = form
+    return render(request, 'account/login.html', context)
--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -23,6 +23,7 @@ from personal.views import (
 from account.views import (
     registration_view,
     logout_view,
+    login_view,
 
 
 )
@@ -34,5 +35,6 @@ urlpatterns = [
     path('', home_screen_view, name='home'),
     path('register/', registration_view, name='register'),
     path('logout/', logout_view, name='logout'),
+    path('login/', login_view, name='login'),
     
 ]
